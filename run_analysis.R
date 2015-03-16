408744e51b34709fdbb1267b3c946610b6e7d233
Abdellah Lahouaoui <abdellah.lahouaoui@gmail.com>
2015-03-16 23:48:05 +0100

    .

--- run_analysis.R
+++ run_analysis.R
@@ -10,35 +10,62 @@
 ##
 
 
-# Loading library handling string trim function
-library(gdata)
+# Loading helpful libraries
+library(gdata)  # Trim function
+library(utils)  # read.table function
 
 #
 # Given a kind of wanted observation return a data frame with tidy data
 #             input: 
-#                 - features : vector of string variable names (regexp) to be output 
-#                 - kind of activity (train/test): string - default=train
+#                 - featToRetrieve : string vector: variable names (regexp) to be output
+#                 - xFileName : string: file name of the observed activity features X_[train|test].txt
+#                 - yFileName : string: file name of the performed activities Y_[train|test].txt
+#                 - sFileName : string: file name of the subject (individual id) performing the activities subject_[train|test].txt
+#                 - obsType: string: type of observation (train/test) - default="train"
+#                 - aFileName: string: file name of the activity labels - default="activity_labels.txt"
+#                 - fFileName: string: file name of the feature names - default="features.txt"
 #             output: 
 #                 - tidy data frame composed of matching features variables names as columns: data.frame 
 #
 #   This function reads:
+#         - activity labels activity_labels.txt file
+#         - features names features.txt file
 #         - features in X_[train|test].txt file
 #         - performed activities in Y_[train|test].txt file
 #         - individuals id in subject_[train|test].txt file
-getActivityObservations = function (features, kind="train"){
+getActivityObservations = function (featToRetrieve, xFileName, yFileName, sFileName, obsKind="train", aFileName="activity_labels.txt", fFileName="features.txt"){
   
-  # readTextFile: read a text file and 
-  #               input: 
-  #                   - string: name of the file to read from
+  # checkFileExistance: check if a file exist.
+  #               input:
+  #                   - fileName: string: name of the file to read from. If using file path, watch out file separator on Windows/Unix worlds
+  #               output: 
+  #                   - boolean: FALSE indicates the file does not exist (an error message is printed out)
+  checkFileExistance(fileName){
+    if (!file.exists(targetFileName)){
+      cat("\t\tUnable to locate file '"+fileName+"'. Aborting analysis!\n")
+      return(FALSE)
+    }
+    return(TRUE)
+  }
+  
+  
+  # getDataVectorFromTextFile: extract a valued vector from a text file 
+  #               input:
+  #                   - fileName: string: name of the file to read from. If using file path, watch out file separator on Windows/Unix worlds
   #               output: 
   #                   - chr vector: read lines 
-  readTxtFile = function(fileName=""){
-    if (fileName=="") return(NULL)
-    conn = file(file.path(kind,fileName), open="r")
-    tmpDF = readLines(conn)
-    if ( nrow(tmpDF) == 0 ) return(NULL)
+  getDataVectorFromTextFile = function(fileName=""){
+    if (!checkFileExistance(fileName)){
+      return(NULL)
+    } 
+    conn = file(targetFileName, open="r")
+    tmpV = readLines(conn)
     close(conn)
-    return (tmpDF)  
+    if (length(tmpV) == 0){
+      cat("\t\t'"+fileName+"' does not contains any value. Aborting analysis!\n")
+      return(NULL)     
+    }
+    return(tmpV)  
   }
 
   # cleanFeaturesObs: Transform a  characteres vector into a clean data.frame
@@ -47,6 +74,94 @@ getActivityObservations = function (features, kind="train"){
   #               output: 
   #                   - data frame
   cleanFeaturesObs = function(obsV){
-    
+    return(unlist(strsplit(trim(obsV),"\\s+")))
   }
+  
+  
+  
+  #
+  # Main routine
+  #
+  
+  print(" Engineering '"+toupper(obsType)+"' observations.")
+  
+  
+  # 1. Read involved individuals IDs (subject_[train|test].txt)
+  cat("\t1.Reading individuals IDs from '"+sFileName+"' file.\n")
+  subjectsID = getDataVectorFromTextFile(sFileName)
+  if (is.null(subjectsID)) return(NULL) 
+  nrSubjectObs = length(subjectsID)
+  cat("\t\tNumbers of observations recorded: ", nrSubjectObs,"\n")
+  cat("\n")
+  
+  
+  # 2. Read performed activity id (Y_[train|test].txt) and Validate that 
+  #    it holds the same number of observations as for Individual IDs
+  cat("\t2.Reading activity IDs from '"+yFileName+"' file.\n")
+  activitiesID = getDataVectorFromTextFile(yFileName)
+  if (is.null(activitiesID)) return(NULL)
+  nrYObs = length(activitiesID)
+  cat("\t\t Numbers of observations recorded: ", nrYObs, "\n")
+  if ( nrYObs != nrSubjectObs ){
+    cat("\t\tObservations' length differs from '"+sFileName+"' to '"+yFileName+"'. Aborting Analysis!\n")
+    return(NULL)
+  }
+  observedActIDLevels = order(as.numeric(levels(factor(activitiesID))))
+  cat("\t\tObserved distinct activity IDs: ",observedActIDLevels,".\n")
+  cat("\n")  
+
+  
+  # 3. Read activity labels (activity_labels.txt) and validate that observed
+  #   activiy IDs are correctly registered within this set.
+  cat("\t3.Reading activity labels from '"+aFileName+"' file.\n")
+  if (!checkFileExistance(aFileName)) return(NULL)
+  actLabels = read.table(aFileName, col.names=c("activityID","activityLabel"))
+  if (is.null(actLabels)){
+    cat("\t\t'"+aFileName+"' does not contains any value. Aborting analysis!\n")
+    return(NULL)
+  }
+  actLabelsIDLevels = order(as.numeric(levels(factor(actLables$activityID)))) 
+  cat("\t\tRegistered activities ID: ",actLabelsIDLevels,".\n")
+  if (!any(observedActIDLevels != actLabelsIDLevels)){
+    cat("\t\tThere is a mismatch between registred activity ID and observed activity ID. Aborting Analysis!\n")
+    return(NULL)
+  }
+  cat("\n")  
+  
+
+  # 4. Read feature names (features.txt) 
+  cat("\t4.Reading feature names from '"+fFileName+"' file.\n")
+  if (!checkFileExistance(fFileName)) return(NULL)
+  featureNames = read.table(fFileName, col.names=c("featureIdx","featureLabel"))
+  if (is.null(featureNames)){
+    cat("\t\t'"+fFileName+"' does not contains any value. Aborting analysis!\n")
+    return(NULL)
+  }
+  nrFeatures = nrow(featuresNames)
+  cat("\t\t Numbers of named features: ", nrFeatures, "\n")
+  cat("\n")
+  
+  
+  # 5. Read measured features (X_[train|test].txt)
+  #    then a. Clean them up
+  #         b. Check that we have the same number of observation according to the number of features
+  #         c. Transform the Vector to a data.frame
+  cat("\t5.Reading observed features from '"+xFileName+"' file.\n")
+  dirtyMeasuredFeatures = getDataVectorFromTextFile(xFileName)
+  if (is.null(dirtyMeasuredFeatures)) return(NULL)
+  # a. Clean the retrieved measured features
+  tidyMeasuredFeatures = cleanFeaturesObs(dirtyMeasuredFeatures)
+  rm(dirtyMeasuredFeatures) # save memory by removing dirtyMeasuresFeatures
+  # b. Check that we have the same number of observation (according to the number of features)
+  #     As a reminder, the total number (length of our vector) should be equal to the number
+  #     of observations (either Subject set or Activity set) times the number of features
+  if (length(tidyMeasuredFeatures)!=nrSubjectObs*nrFeatures){
+    cat("\t\tFound ",length(tidyMeasuredFeatures)," measures, which does not equal ",nrSubjectObs,"x",nrFeatures,".")
+    cat("Aborting Analysis!")
+    return(NULL)
+  }
+  # c. Transform the Vector to a data.frame
+  tidyDF = data.frame(matrix(as.numeric(tidyMeasuredFeatures), nrow=nrSubjectObs))
+  
+  
 }
